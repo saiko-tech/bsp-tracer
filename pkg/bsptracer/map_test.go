8ad30267001a41d474aa3aebfcc280192a4b9f4a
Casey Mulcahy <caseymulcahy@proton.me>
2022-06-25 13:47:36 -0400

    Test for correct length of loaded lumps

--- pkg/bsptracer/map_test.go
+++ pkg/bsptracer/map_test.go
@@ -20,5 +20,15 @@ func TestLoadMap(t *testing.T) {
 	// test loading good file
 	m, err = LoadMap("../../testdata", "de_cache.bsp")
 	assert.Nil(t, err)
-	// TODO test for same object totals as C++ library on same file
+	assert.Equal(t, 5560, len(m.brushes))
+	assert.Equal(t, 39815, len(m.brushSides))
+	assert.Equal(t, 129415, len(m.edges))
+	assert.Equal(t, 24072, len(m.leafBrushes))
+	assert.Equal(t, 18843, len(m.leafFaces))
+	assert.Equal(t, 8906, len(m.leaves))
+	assert.Equal(t, 8648, len(m.nodes))
+	assert.Equal(t, 30626, len(m.planes))
+	assert.Equal(t, 23221, len(m.surfaces))
+	assert.Equal(t, 185200, len(m.surfEdges))
+	assert.Equal(t, 48496, len(m.vertices))
 }
