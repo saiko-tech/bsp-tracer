3bf9d0f53b3d25b437bd463df74801e2a56986b2
Markus <mark@saiko.tech>
2022-07-04 00:07:00 +0100

    fix testdata ref

--- testdata/download.sh
+++ testdata/download.sh
@@ -3,4 +3,4 @@
 set -o pipefail
 cd $(dirname $0)
 
-git cat-file blob testdata:testdata/de_cache.bsp | git lfs smudge > de_cache.bsp
+git cat-file blob origin/testdata:testdata/de_cache.bsp | git lfs smudge > de_cache.bsp
